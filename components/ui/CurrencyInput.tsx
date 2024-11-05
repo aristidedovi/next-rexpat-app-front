9eedf14ecda3d6ab36d7f5b5961575752eff679d
Aristide <dovi.aristide@gmail.com>
2024-11-05 14:13:41 +0100

    update IG form view

--- components/ui/CurrencyInput.tsx
+++ components/ui/CurrencyInput.tsx
@@ -10,11 +10,6 @@ export default function CurrencyInput({
   inputDeviseValue,
   inputDeviseName,
 }: any) {
-  useEffect(() => {
-    if (inputDeviseValue === "") {
-    }
-  });
-
   const inputClassName =
     "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-md focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
 
