3bfec1c7c891a1e349ef1175987aa52a7b147a0f
Aristide <dovi.aristide@gmail.com>
2024-12-11 16:16:01 +0100

    update candidat form

--- app/service-a-domicile/candidate/components/CandidatForm.tsx
+++ app/service-a-domicile/candidate/components/CandidatForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { div } from "motion/react-client";
+//import { div } from "motion/react-client";
 // import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 // import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
